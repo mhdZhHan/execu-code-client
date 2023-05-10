cd246fc306a2e908febbc653c109ff95af7ae2bd
Mohammed Shajahan <mohammedsh6236@gmail.com>
2023-05-10 16:25:34 +0530

    Fix language selection

--- src/components/screens/Editor.jsx
+++ src/components/screens/Editor.jsx
@@ -7,17 +7,35 @@ import '../../themes/editor.scss'
 function Editor() {
     const [code, setCode] = useState('')
     const [output, setOutput] = useState('')
+    const [error, setError] = useState('')
+
+    const [languages] = useState([
+        {value: 'cpp', text: 'C++'},
+        {value: 'python', text: 'Python'},
+    ])
+    const [selectedLanguage, setselectedLanguage] = useState(languages[0].value)
+
+    const handleLanguageChange = (event) => {
+        setselectedLanguage(event.target.value)
+    }
+
+    console.log("selected",selectedLanguage)
 
     const codeSubmit = async () => {
+        console.log(selectedLanguage);
         const payload = {
-            language: 'cpp',
+            language: selectedLanguage,
             code
         }
 
         await axios.post('http://localhost:9000/api/v1/compiler/run/', payload)
             .then((response)=> {
                 // console.log(response)
-                setOutput(response?.data.output)
+                response?.data?.error?.stdError ? 
+                    setError(response?.data?.error?.stdError) : setError('')
+
+                response?.data?.output ?
+                     setOutput(response?.data?.output) : setOutput('')
             })
             .catch((error)=> {
                 console.log(error)
@@ -28,6 +46,22 @@ function Editor() {
         <div className="container">
             <div className="wrapper">
                 <h1 className="title">Execu Code Demo</h1>
+                <div className="select_box">
+                    <label htmlFor="for_select">Select language: </label>
+                    <select 
+                        id='for_select'
+                        value={selectedLanguage} 
+                        onChange={handleLanguageChange}
+                    >
+                        {languages.map((language, index ) => (
+                            <option 
+                                key={index} 
+                                value={language?.value}
+                            >{language?.text}</option>
+                        ))}
+                    </select>
+                </div>
+
                 <div className="editor_container">
                     <textarea 
                         className='editor_box'
@@ -51,7 +85,7 @@ function Editor() {
                     </div>
 
                     <div className="error_box">
-                        error occoure
+                        {error && error}
                     </div>
                 </div>
             </div>
